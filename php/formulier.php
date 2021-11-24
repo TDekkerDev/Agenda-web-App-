b42d58c5a8d650717dcf2a1da5ec17bba21da59a
SimonNes112 <164831@student.horizoncollege.nl>
2021-11-24 11:42:26 +0100

    i;u4rtwUO'4WRUIOT45R3Wu8ij34eqr7yuh8945uyhiuio4352ruioh34tuoh453rtou3t42oui4r23tou8t53roiu8

--- php/formulier.php
+++ php/formulier.php
@@ -1,9 +1,24 @@
 <?php 
 include "../php/header.php"; ?>
-<form action="uitkomst.php" method="$_POST">
-    <label for="titel">Titel Afspraak</label>
-    <input type="text" name="titel">
+<form action="uitkomst.php" method="POST">
+    <h1>Nieuwe Agenda Item</h1>
 
+    <label for="titel">Titel:
+        <input type="text" name="titel"> <br>
+    </label>
+    
+    <label for="afspraak">Omschrijving van de afspraak:
+        <input type="text" name="afspraak" id="omschrijving"> <br>
+    </label>
+
+    <label for="locatie">Locatie:
+        <input type="text" name="locatie" id="locatie1">
+    </label>
+
+    <label for="date">Datum van afspraak:
+        <input type="date" name="date" id="datum">
+    </label>
+    
 
 
 
---
+++ php/uitkomst.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+
+$error=0;
+if(empty($title)){
+    $error=1;
+}
+
+if ($error ==0){
+    echo $title;
+    
+}
+
+?>
