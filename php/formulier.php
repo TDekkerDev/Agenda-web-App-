70eba31cba4784c0831cd156ff27a66b1d5ac06c
SimonNes112 <164831@student.horizoncollege.nl>
2021-11-24 10:38:44 +0100

    jkngtr4wjkntr4g5wjnkljklnwt

---
+++ php/formulier.php
@@ -0,0 +1,19 @@
+<?php 
+include "../php/header.php"; ?>
+<form action="uitkomst.php" method="$_POST">
+    <label for="titel">Titel Afspraak</label>
+    <input type="text" name="titel">
+
+
+
+
+
+
+
+
+</form>
+
+
+
+<?php 
+include "../php/footer.php"; ?>
