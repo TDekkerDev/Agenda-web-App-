504e5b4d38bed566f3eb551c53f4ffc8d2cb6e8b
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-11-24 11:58:52 +0100

    werking

--- php/navbar.php
+++ php/navbar.php
@@ -1,3 +1,9 @@
-
-
-<img src=""
+<header>
+        <img class="logo" src="media/logo.png" alt="logo">
+        <ul class="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+        </ul>
+        <a class="button" href="#"><button>Contact</button></a>
+    </header>
