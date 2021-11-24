0b07771f5138b8dd00ff63749304e44aa4674836
SimonNes112 <164831@student.horizoncollege.nl>
2021-11-24 13:25:29 +0100

    h

--- uitkomst.php
+++ uitkomst.php
@@ -1,7 +1,11 @@
 <?php
 
-
-
+$titel = $_POST["titel"];
+$afspraak = $_POST["afspraak"];
+$locatie = $_POST["locatie"];
+$date = $_POST["date"];
+$begintijd =$_POST["time-begin"];
+$eindtijd = $_POST["time-eind"];
 
 
 
