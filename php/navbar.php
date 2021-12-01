77706875f768fb4a7b61f2c92008abe47e954ba6
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-12-01 10:21:50 +0100

    ya

--- php/navbar.php
+++ php/navbar.php
@@ -1,8 +1,8 @@
 <header>
         <img class="logo" src="media/logo4.png" alt="logo">
         <ul class="nav">
-            <li class="navi"><a href="formulier.php">Home</a></li>
-            <li class="navi"><a href="test.php">About</a></li>
+            <li class="navi"><a href="formulier.php">agenda</a></li>
+            <li class="navi"><a href="test.php">Afspraken</a></li>
             <li class="navi"><a href="#">Services</a></li>
         </ul>
         <a class="button" href="#"><button>Contact</button></a>
