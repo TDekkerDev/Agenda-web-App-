3406d29b7dbdc971ae400640123f82a028963282
SimonNes112 <164831@student.horizoncollege.nl>
2021-12-01 12:58:29 +0100

    homo

--- uitkomst.php
+++ uitkomst.php
@@ -17,41 +17,71 @@ $error=0;
 $timestamp= strtotime($date);
 
 
+function save($titel,$afspraak,$locatie,$date,$begintijd,$eindtijd){
+
+    $file_path ="data/saved_item.json";
+    if (file_exists($file_path)){
+        $saved_items = file_get_contents($file_path);
+        $saved_items = json_decode($saved_items,true);
+    }else{
+        $saved_items = [];
+
+    }
+
+    $new_item = [
+        "title" => $titel,
+        "afspraak" => $afspraak,
+        "locatie" => $locatie,
+        "datum" => $date,
+        "begintijd" => $begintijd,
+        "eindtijd" => $eindtijd
+    ];
+    $saved_items[]= $new_item;
+
+    $saved_items_json = json_encode($saved_items);
+    file_put_contents($file_path,$saved_items_json);
+
+    print_r($saved_items_json);
+    
+    return $saved_items;
+    
+    
+}
 
 
 if(empty($titel)){
     
-    header('Location:formulier.php?error=U moet Titel invoeren');
+    header('Location:index.php?error=U moet Titel invoeren');
 }
 
 if(empty($afspraak)){
     
-    header('Location:formulier.php?error=U moet de omschijving van de afspraak invoeren');
+    header('Location:index.php?error=U moet de omschijving van de afspraak invoeren');
 }
 
 
 if(empty($locatie)){
     
-    header('Location:formulier.php?error=U moet locatie invoeren');
+    header('Location:index.php?error=U moet locatie invoeren');
 }
 
 
 if(empty($date)){
     
-    header('Location:formulier.php?error=U moet de datum invoeren');
+    header('Location:index.php?error=U moet de datum invoeren');
 }elseif(time()>$timestamp){
-    header('Location:formulier.php?error=U moet een datum kiezen die nog niet is geweest');
+    header('Location:index.php?error=U moet een datum kiezen die nog niet is geweest');
 
 }
 
 if(empty($begintijd)){
     
-    header('Location:formulier.php?error=U moet de begintijd invoeren');
+    header('Location:index.php?error=U moet de begintijd invoeren');
 }
 
 if(empty($eindtijd)){
     
-    header('Location:formulier.php?error=U moet de eindtijd invoeren');
+    header('Location:index.php?error=U moet de eindtijd invoeren');
  
 
 }
@@ -68,6 +98,7 @@ if($error==0){
     echo $begintijd;
     echo "<br>";
     echo $eindtijd;
+    save($titel,$afspraak,$locatie,$date,$begintijd,$eindtijd);
 }
 
 
