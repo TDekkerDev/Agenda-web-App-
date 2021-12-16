a2f20823be323f1d97ec13e26aa07a347abf11e3
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-12-16 09:43:01 +0100

    ww

--- zoeken.php
+++ zoeken.php
@@ -3,36 +3,17 @@
 <?php
 $zoeken_naam = $_POST["zoeken"];
 $zoeken_term = $_POST["zoekenkeuzen"];
-
-
-function save($saved_item){
-    
-
+$saved_items=getcontent();
+function getcontent(){ 
     $file_path ="data/saved_item.json";
-    if (file_exists($file_path)){
-        $saved_items = file_get_contents($file_path);
-        $saved_items = json_decode($saved_items,true);
-    }else{
-        $saved_items = [];
+        if (file_exists($file_path)){
+            $saved_items = file_get_contents($file_path);
+            $saved_items = json_decode($saved_items,true);
+        }else{
+            $saved_items = [];
 
     }
-
-    $saved_items_json = json_encode($saved_items);
-    file_put_contents($file_path,$saved_items_json);
-
-    
-
-    
-    foreach($saved_items as $item){
-     
-        echo   "<br>".$item["title"]  . "<br>". $item["afspraak"]  . "<br>". $item["locatie"] . "<br>" . $item["datum"] . "<br>" . $item["begintijd"]  . "<br>". $item["eindtijd"] . "<br>";
-        
-
-    }
-    
-    
     return $saved_items;
-    
     
 }
 
@@ -41,13 +22,13 @@ echo "<br>";
 echo "met de egenschaep: $zoeken_term";
 echo "<br>";
 
-    $array_column = array_column($saved_items, 'title');
+    $saved_items = array_column($saved_items, 'title');
     
-    $name_ding = array_search($zoeken_naam, $array_column);
+    $name_ding = array_search($zoeken_naam, $saved_items);
     
-    echo $array[$name_ding]['title'];
+    echo $saved_items[$name_ding];
     echo "<br>";
-print_r(save());
+//print_r(getcontent());
 
 ?>
 <?php include "php/footer.php";?>
